28f5c557215e6e4310e4d795800509aa869cefb1
dealvox-lab <ongo.promotion@gmail.com>
2026-02-13 09:36:36 +0300

    Update next.config.js

--- web-app/next.config.js
+++ web-app/next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',  // Add this line
+  images: {
+    unoptimized: true,  // Add this line
+  },
   reactStrictMode: true,
 }
 
